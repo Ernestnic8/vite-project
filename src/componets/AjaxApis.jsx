3dfedd24b41e74e68465cf5f0fc414d2e99d24ea
Developer <develop@lafise.com>
2024-10-08 08:17:28 -0600

    ingresos

--- src/componets/AjaxApis.jsx
+++ src/componets/AjaxApis.jsx
@@ -1,56 +1,68 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function Pokemon(props){
-    return(
+function Pokemon({ avatar, name }) {
+    return (
         <figure>
-            <image src={props.avatar} alt={props.name}/>
-            <figcaptin>{props.name}</figcaptin>
+            <img src={avatar} alt={name} />
+            <figcaption>{name}</figcaption>
         </figure>
     );
 }
 
-export default class AjaxApis extends Component{
+// Parámetros por defecto
+Pokemon.defaultProps = {
+    avatar: 'url_por_defecto',
+    name: 'Nombre por defecto',
+};
 
-    state={
-        pokemons:[],
+export default class AjaxApis extends Component {
+    state = {
+        pokemons: [],
     }
 
     componentDidMount() {
-        let url="https://pokeapi.co/api/v2/pokemon"
+        let url = "https://pokeapi.co/api/v2/pokemon";
         fetch(url)
-        .then(res => res.json())
-        .then((json) => {
-            console.log(json);
-            json.results.forEach((el) =>{fetch(el.url).then((res) => res.json()).then(json => {console.log(json)
-                let pokemon = {
-                    id:json.id,
-                    name:json.name,
-                    avatar:json.sprites.front_default,
-                };
+            .then(res => res.json())
+            .then((json) => {
+                console.log(json);
+                const fetchPromises = json.results.map(el => {
+                    return fetch(el.url)
+                        .then(res => res.json())
+                        .then(json => {
+                            console.log(json);
+                            return {
+                                id: json.id,
+                                name: json.name,
+                                avatar: json.sprites.front_default,
+                            };
+                        });
+                });
 
-                let pokemons = {...this.state.pokemons, pokemon};
-
-                this.setState({pokemons})
-            });
+                // Esperamos a que todas las promesas se resuelvan
+                Promise.all(fetchPromises).then(pokemons => {
+                    this.setState({ pokemons });
+                });
             })
-        })
+            .catch(error => console.error('Error fetching Pokémon:', error));
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-              <h2>AJAX con APIs</h2>
-              {this.state.pokemons === 0 ? (<h3>Cargando...</h3>) : 
-              (
-                this.state.pokemons.map((el)=> (<Pokemon key={el.id} name={el.name} avatar={el.avatar}/>))
-              )}
+                <h2>AJAX con APIs</h2>
+                {this.state.pokemons.length === 0 ? (<h3>Cargando...</h3>) :
+                    (
+                        this.state.pokemons.map((el) =>
+                            (<Pokemon key={el.id} name={el.name} avatar={el.avatar} />))
+                    )}
             </>
-        )
+        );
     }
 }
 
-Pokemon.PropTypes = {
-    avatar: PropTypes.image,
+Pokemon.propTypes = {
+    avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-}
+};
